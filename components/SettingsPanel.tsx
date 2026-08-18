b73c25a2e71b902e367efc0dc2c981505128c4a3
hapy9389-prog <hapy9389@gmail.com>
2026-08-18 12:26:29 +0900

    토글 위치 수정

--- components/SettingsPanel.tsx
+++ components/SettingsPanel.tsx
@@ -67,8 +67,8 @@ export function SettingsPanel({
               }`}
             >
               <span
-                className={`absolute top-0.5 h-5 w-5 rounded-full bg-paper shadow transition-transform ${
-                  crossCharacterInteractionEnabled ? "translate-x-[22px]" : "translate-x-0.5"
+                className={`absolute left-0.5 top-0.5 h-5 w-5 rounded-full bg-paper shadow transition-transform ${
+                  crossCharacterInteractionEnabled ? "translate-x-5" : "translate-x-0"
                 }`}
               />
             </button>
